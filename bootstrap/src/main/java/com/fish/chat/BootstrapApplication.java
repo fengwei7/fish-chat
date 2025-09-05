d102fbb524eadcf301612de4b45890d4606ebb42
Wei <fengwei5207@outlook.com>
2025-09-05 14:23:32 +0800

    启动成功后打印banner

--- bootstrap/src/main/java/com/fish/chat/BootstrapApplication.java
+++ bootstrap/src/main/java/com/fish/chat/BootstrapApplication.java
@@ -1,13 +1,23 @@
 package com.fish.chat;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.fish.chat.mapper.mysql")
 public class BootstrapApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BootstrapApplication.class, args);
+        System.out.println(" _____                _    ______ _     _     \n"
+            + "|_   _|              | |   |  ___(_)   | |    \n"
+            + "  | | ___  _   _  ___| |__ | |_   _ ___| |__  \n"
+            + "  | |/ _ \\| | | |/ __| '_ \\|  _| | / __| '_ \\ \n"
+            + "  | | (_) | |_| | (__| | | | |   | \\__ \\ | | |\n"
+            + "  \\_/\\___/ \\__,_|\\___|_| |_\\_|   |_|___/_| |_|\n"
+            + "                                              \n"
+            + "                                              ");
     }
 
 }
---
+++ bootstrap/src/main/java/com/fish/chat/mapper/mysql/UserMapper.java
@@ -0,0 +1,10 @@
+package com.fish.chat.mapper.mysql;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.fish.chat.entity.User;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface UserMapper extends BaseMapper<User> {
+
+}
