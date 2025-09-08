82b79dd6ca652fa0abc50db680179b93c5cd3a5d
Wei <fengwei5207@outlook.com>
2025-09-08 15:41:47 +0800

    照抄sa-token SaResult写了一个Result类

--- bootstrap/src/main/java/com/fish/chat/controller/LoginController.java
+++ bootstrap/src/main/java/com/fish/chat/controller/LoginController.java
@@ -1,7 +1,13 @@
 package com.fish.chat.controller;
 
+import cn.dev33.satoken.stp.StpUtil;
+import com.fish.chat.dto.UserReqDTO;
 import com.fish.chat.service.UserService;
+import com.fish.chat.utils.result.Result;
+import java.util.HashMap;
+import java.util.Map;
 import javax.annotation.Resource;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +17,13 @@ public class LoginController {
 
     @Resource
     UserService userService;
+
+    @PostMapping("/login")
+    public Result login(UserReqDTO user) {
+        StpUtil.login(user.getId());
+        Map<String, Object> map = new HashMap<>();
+        map.put("token", StpUtil.getTokenValue());
+        return Result.data(map);
+    }
 
 }
--- bootstrap/src/main/java/com/fish/chat/dto/UserReqDTO.java
+++ bootstrap/src/main/java/com/fish/chat/dto/UserReqDTO.java
@@ -1,5 +1,17 @@
 package com.fish.chat.dto;
 
+import com.baomidou.mybatisplus.annotation.TableField;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserReqDTO {
+    private Long id;
+    private String username;
+    private String password;
+    private String mobile;
 
 }
