9b76e0a1f0d8d001a658b7e25af975e6fa598ba8
Wei <fengwei5207@outlook.com>
2025-09-10 16:29:59 +0800

    动态全局拦截器

--- bootstrap/src/main/java/com/fish/chat/config/SaTokenConfigure.java
+++ bootstrap/src/main/java/com/fish/chat/config/SaTokenConfigure.java
@@ -1,7 +1,10 @@
 package com.fish.chat.config;
 
 import cn.dev33.satoken.interceptor.SaInterceptor;
+import cn.dev33.satoken.router.SaRouter;
 import cn.dev33.satoken.stp.StpUtil;
+import java.util.Arrays;
+import java.util.List;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -10,10 +13,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class SaTokenConfigure implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        // 注册 Sa-Token 拦截器，校验规则为 StpUtil.checkLogin() 登录校验。
-        registry.addInterceptor(new SaInterceptor(handle -> StpUtil.checkLogin()))
-            .addPathPatterns("/**")
-            .excludePathPatterns("/auth/register")
-            .excludePathPatterns("/auth/login");
+        registry.addInterceptor(new SaInterceptor(handle -> {
+            SaRouter
+                .match("/**")
+                .notMatch(excludePaths())
+                .check(r -> StpUtil.checkLogin());
+        })).addPathPatterns("/**");
+    }
+
+    // 动态获取哪些 path 可以忽略鉴权
+    public List<String> excludePaths() {
+//        return Arrays.asList("/path1", "/path2", "/path3");
+        return Arrays.asList("/auth/login","/auth/register");
     }
 }
